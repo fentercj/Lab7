36f0540dd6eccdd45fb975bcf806c344d90d219b
fentercj <fentercj@mail.uc.edu>
2021-03-22 16:23:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+//My partner for Lab7 is Jonathan Ashbrook
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Main{
+  public static void main(String args[]){
+    SwingUtilities.invokeLater(new Runnable(){
+      public void run(){
+        new Counter();
+      }
+    });
+  }
+}
